76aeac7374285c339f7e645dfbeb1a981cf3905b
Santiago Henares <santiagodhenares@gmail.com>
2022-08-11 21:04:03 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Santiago Henares</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido a mi tiendita online, sientate y elige tu compra.
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Don´t learn React
         </a>
       </header>
     </div>
